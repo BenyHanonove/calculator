cd1d2cc055d2f9d10376ec83090ce53dcda539fb
benyx13 <benyyoulove@gmail.com>
2023-05-16 18:06:31 +0300

    screen for calculator

--- src/components/CalculatorBody/CalculatorBody.js
+++ src/components/CalculatorBody/CalculatorBody.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Container } from "react-bootstrap";
+import CalculatorScreen from '../CalculatorScreen/CalculatorScreen';
 import "./CalculatorBody.css";
+
 
 function CalculatorBody() {
   return (
     <div className='page-wrapper'>
         <Container className='calculator'>
-        
+            <CalculatorScreen/>
         </Container>
     </div>
   )
---
+++ src/components/CalculatorScreen/CalculatorScreen.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {Row} from "react-bootstrap";
+import "./CalculatorScreen.css";
+
+function CalculatorScreen() {
+
+    const [screenValue ,setScreenValue] = useState("123")
+
+
+  return (
+    <Row className='CalculatorScreen'>
+        <label className='ScreenText'>{screenValue}</label>
+    </Row>
+  )
+}
+
+export default CalculatorScreen;
